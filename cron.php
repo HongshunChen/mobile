7005bc5c895e7cd54cdbe8ced31198f224259f76
unknown <dl.hongshun@qq.com>
2015-11-22 23:13:17 +0800

    crontab

--- cron.php
+++ cron.php
@@ -1,8 +1,29 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// 让crontab定时执行的脚本程序     */5 * * * * /usr/bin/php /data/www/app/cron.php
 
+// 想获取video中 6条数据
+
+require_once('./db.php');
+require_once('./Cache.php');
+
+$sql = "select * from t_user";
+try {
+	$connect = Db::getInstance()->connect();
+} catch(Exception $e) {
+	// $e->getMessage();
+	file_put_contents('./logs/'.date('y-m-d') . '.txt' , $e->getMessage());
+	return;
+}
+$result = mysql_query($sql, $connect); 
+$rows = array();
+while($row = mysql_fetch_assoc($result)) {
+	$rows[] = $row;
+}
+$file = new Cache();
+if($rows) {
+	$file->cacheData('index_cron_cahce', $rows);
+} else {
+	file_put_contents('./logs/'.date('y-m-d') . '.txt' , "没有相关数据");
+}
+return;
